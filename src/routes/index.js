18b07ad032643e7cfdf233f283a0ab1ded719907
Hamid <hamidarianyegane@gmail.com>
2023-08-31 23:58:34 +0330

    handle server errors

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const authRouter = require("./auth");
 const userRouter = require("./user");
 const adminRouter = require("./admin");
-const { isLoggedIn, isAdmin } = require("./../middlewares/auth");
+const { isLoggedIn, isAdmin } = require("../middlewares/auth");
+const error = require("../middlewares/error");
 
 router.use("/auth", authRouter);
 router.use("/user", isLoggedIn, userRouter);
 router.use("/admin", isLoggedIn, isAdmin, adminRouter);
+router.use(error);
+
 module.exports = router;
